188acefbc7eda286d869fde5267694d88caf3b58
henrique <henriquepro8@gmail.com>
2023-09-20 12:00:04 -0300

    controller

---
+++ Fiscalio/Controllers/NotaFiscalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Fiscalio.Models; // Substitua pelo namespace correto do seu modelo
+using Fiscalio.Persistence; // Substitua pelo namespace correto do seu contexto de banco de dados
+using System.Linq;
+
+namespace Fiscalio.Controllers
+{
+    public class NotaFiscalController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public void NotaFiscaisController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
